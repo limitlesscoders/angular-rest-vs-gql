34fe54c63348bf362586f47c9a4284351ad9048e
limitlesscoders <limitlesscoders@gmail.com>
2021-04-28 15:43:39 -0700

    added values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,21 @@ import {Apollo, gql} from 'apollo-angular';
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  public restData: RestData[] = [];
-  rates: any[];
-  loading = true;
-  error: any;
+  public restData: Data[] = [];
+  public graphQlData: Data[] = [];
   constructor(private http: HttpClient, private apollo: Apollo) {
+    // REST API fetch, serer has all the control on what data to present
     this.http
-      .get("https://jsonplaceholder.typicode.com/users")
+      .get("https://jsonplaceholder.typicode.com/posts")
       .toPromise()
-      .then((response: any) => {
-        this.restData = response;
-        console.log("console ", response);
+      .then((response: any) => {                
+        if(response && response.length >0){
+          this.restData = response.filter((d: Data)=> d.id <= 5);;
+          console.log("REST API DATA - ",this.restData);
+        }        
       });
 
+// Graphql Fetch, User has all the control what date needs to be fetched
     this.apollo
       .watchQuery({
         query: gql`
@@ -34,21 +36,20 @@ export class AppComponent {
         }
         `,
       })
-      .valueChanges.subscribe((result: any) => {
-        console.log('RESULT ', result);
-        this.rates = result?.data?.rates;
-        this.loading = result.loading;
-        this.error = result.error;
+      .valueChanges.subscribe((result: any) => {        
+        if(result && result.data) {
+          // we only want to display 5 entries
+          this.graphQlData = (result.data.posts?.data).filter((d: Data)=> d.id <= 5);
+          console.log('Graphql DATA - ', this.graphQlData);
+        }
+        
       });
   }
 }
 
-export interface RestData {
+export interface Data {
   id: number;
-  address: any;
-  name: string;
-  username: string;
-  email: string;
-  phone: string;
-  website: string;
+  title: string;
+  body: string;
+  userId: number;
 }
